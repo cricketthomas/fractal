31dc1ee281557cd09eb0adadff603eac107e780e
Arthur Thomas <athomas@sandscap.com>
2022-07-25 11:57:43 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+
+
+
+var coords = new List<Complex>() { };
+
+var c = 2;
+var range = Enumerable.Range(0, 100);
+var previousZval = 0;
+
+
+foreach(var num in range)
+{
+    var z = Math.Sqrt(num) + c;
+    if(previousZval > 0)
+        coords.Add(z);
+    Console.WriteLine(z);
+
+
+
+}
+
+Console.ReadKey();
